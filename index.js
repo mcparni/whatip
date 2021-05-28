c371ecb7163276029cfb4d8f88e0a8c65ccc63c9
Miika Pärni <miika.parni@helsinki.fi>
2021-05-28 11:17:03 +0300

    Main code for the app

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const app = express()
+let givenPort;
+let port = 3000;
+app.set('trust proxy', true)
+
+process.argv.forEach(function (val, index, array) {
+  if(index === 2) {
+    givenPort = Number(val);
+    givenPort = (isNaN(givenPort) || (givenPort < 0 || givenPort > 65535)) ? port : Number(val); 
+  }
+});
+
+app.get('/', function (req, res) {
+  let  ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }
+  let html = `<!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>What IP</title>
+    </head>
+    <body>
+      <div>Your ip address is ${ip}</div>
+    </body>
+  </html>`
+  res.status(200).send(html);
+})
+ 
+app.listen(givenPort)
+console.log(`App listening on port ${givenPort}`);
